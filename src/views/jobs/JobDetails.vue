49fb853fd4973a788812438cde4f105899fc8238
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-13 10:36:15 +0100

    get and display single job

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,26 @@
 <template>
-    <h1>Job Details page</h1>
-    <p>The job id is: {{ id }}</p>
+    <div v-if="job">
+        <h1>{{job.title}}</h1>
+        <p>The job id is: {{ job.id }}</p>
+        <p>The job Detail is: {{ job.details }}</p>
+    </div>
+    <h1 v-else>Loading...</h1>
 </template>
 
 <script>
     export default {
         props: ['id'],
-        // data() {
-        //     return {
-        //         id: this.$route.params.id
-        //     }
-        // }
+        data() {
+            return {
+                job: null
+            }
+        },
+        mounted() {
+            fetch(`http://localhost:3000/jobs/${this.id}`)
+                .then(res => res.json())
+                .then(data => this.job = data)
+                .catch(err => console.log(err))
+        }
     }
 </script>
 
